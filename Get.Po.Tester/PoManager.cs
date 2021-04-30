5217351a473ca309114d22ed8de526958f0edcc9
Dinusha Gamage <dpm@tiqri.com>
2021-04-30 12:56:49 +0530

    Tester

---
+++ Get.Po.Tester/PoManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Get.Po.Tester
+{
+   public static class PoManager
+    {
+        public static async void MainAsyncFromApim(string url, int iteration)
+        {
+            Console.WriteLine($"Fetching iteration {iteration}");
+            string authToken = GetAuthToken();
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                    request.Headers.Add("Authorization", "Bearer " + authToken);
+                    request.Headers.Add("Ocp-Apim-Subscription-Key", "dceaef0f1af741ca93eca8122034009c");
+                    HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
+                    string responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    Console.WriteLine($"========Reequest No.:{iteration}.{i}============================================================");
+                    Console.WriteLine(responseString);
+                    Console.WriteLine();
+                    Console.WriteLine($"===============================================================================================");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+        }
+
+        public static async void MainAsync(string url, int iteration)
+        {
+            Console.WriteLine($"Fetching iteration {iteration}");
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage[] result = new HttpResponseMessage[5];
+            HttpResponseMessage response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            Console.WriteLine($"========Reequest No.:{iteration}==============================================================");
+            Console.WriteLine(responseString);
+            Console.WriteLine();
+            Console.WriteLine($"===============================================================================================");
+
+            //return await Task.FromResult(result);
+        }
+
+
+
+        private static string GetAuthToken()
+        {
+            return "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiIwMDAwMDAwMi0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC80NmM1MTc4ZS1hMGY0LTRmNGQtOGM0MC05NTk4ZTNkMTE4NjAvIiwiaWF0IjoxNjE5NzYzNTU5LCJuYmYiOjE2MTk3NjM1NTksImV4cCI6MTYxOTc2NzQ1OSwiYWlvIjoiRTJaZ1lHajg1QlJ1M3ZrL2ZGZjNvNWtId3RwOUFRPT0iLCJhcHBpZCI6ImU4NjcyNmUxLWExMzEtNDFhMC05NTc2LWU5MzE1Y2E3MWUwMCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzQ2YzUxNzhlLWEwZjQtNGY0ZC04YzQwLTk1OThlM2QxMTg2MC8iLCJvaWQiOiJiMmM5MDk2Yi0xMTRiLTRjNjQtOTU0Ny01MjBmYjEzODk1ZTYiLCJyaCI6IjAuQVFNQWpoZkZSdlNnVFUtTVFKV1k0OUVZWU9FbVotZ3hvYUJCbFhicE1WeW5IZ0FEQUFBLiIsInN1YiI6ImIyYzkwOTZiLTExNGItNGM2NC05NTQ3LTUyMGZiMTM4OTVlNiIsInRlbmFudF9yZWdpb25fc2NvcGUiOiJOQSIsInRpZCI6IjQ2YzUxNzhlLWEwZjQtNGY0ZC04YzQwLTk1OThlM2QxMTg2MCIsInV0aSI6IjBmaXdXY2c2RWt1X0JYb3A0OXhDQVEiLCJ2ZXIiOiIxLjAifQ.la48EigzZXT437i2XGG-ks5TtMDHedo7XpSnCLe8rfvZs_P4rhK5yCwZxZ884YRPs4kPVW2P9qbiW9rgxv5z6LrlqBZR98dspocZrEY32YYUuBlpo0rmae70zk4kqOjVAQzZLQZjWdhuDGzkn1kkM5BbODgRmYc_2Jw8FsaxUGy0QZXonNGpJmqMDlbw3irJy7uhNkiYT5E5G6pUuvWGHFtM8pRG7_RWfedcR9xX2mptfwKV8V4JUUbuJRh7O9skqy-O5tRwbco5HnNpjTkO6w24MR7XjN6CQloN1Oi_7S9EA3qpDFDGM7xVsQQLcmVfXxFbZGNxS8eKnLPjH-QSWA";
+
+        }
+    }
+}
---
+++ Get.Po.Tester/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Get.Po.Tester
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            //string apiUrlWithParams = "https://cw-po-api-osb-dev.azurewebsites.net/api/purchase-orders?PONo=94122";
+            string apiUrlWithParams = "https://integrations-dev.cushwake.com/po-api-dev/api/purchase-orders?PONo=94122";
+           
+                //Token Generation code
+             RunAsync().ConfigureAwait(false);
+            
+            Console.WriteLine($"API URL={apiUrlWithParams}");
+            for (int i = 0; i < 3; i++)
+            {
+                PoManager.MainAsyncFromApim(apiUrlWithParams, i + 1);
+            }
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+
+       
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = new AuthenticationConfig();
+
+            config.Instance = "https://login.microsoftonline.com/{0}/oauth2/token";
+            config.Tenant = "46c5178e-a0f4-4f4d-8c40-9598e3d11860";
+            config.ClientId = "6731de76-14a6-49ae-97bc-6eba6914391e";
+            config.ClientSecret = "D-5rEw.~6Ry8lt.X_013-8xcq9h74aQhCt";
+            //config.Authority = "";
+
+            // You can run this sample using ClientSecret or Certificate. The code will differ only when instantiating the IConfidentialClientApplication
+            //bool isUsingClientSecret = AppUsesClientSecret(config);
+
+            // Even if this is a console application here, a daemon application is a confidential client application
+            IConfidentialClientApplication app;
+
+            //if (isUsingClientSecret)
+            //{
+                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                    .WithClientSecret(config.ClientSecret)
+                    .WithAuthority(new Uri(config.Authority))
+                    .Build();
+            //}
+
+            //else
+            //{
+            //    X509Certificate2 certificate = ReadCertificate(config.CertificateName);
+            //    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+            //        .WithCertificate(certificate)
+            //        .WithAuthority(new Uri(config.Authority))
+            //        .Build();
+            //}
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator. 
+            string[] scopes = new string[] { $"{config.ApiUrl}.default" };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) //when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                Console.WriteLine($"AuthToken:{JsonConvert.SerializeObject(result)}");
+                Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            }
+        }
+
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        /// </summary>
+        /// <param name="config">Configuration from appsettings.json</param>
+        /// <returns></returns>
+        //private static bool AppUsesClientSecret(AuthenticationConfig config)
+        //{
+        //    string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
+        //    string certificatePlaceholderValue = "[Or instead of client secret: Enter here the name of a certificate (from the user cert store) as registered with your application]";
+
+        //    if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
+        //    {
+        //        return true;
+        //    }
+
+        //    else if (!String.IsNullOrWhiteSpace(config.CertificateName) && config.CertificateName != certificatePlaceholderValue)
+        //    {
+        //        return false;
+        //    }
+
+        //    else
+        //        throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+        //}
+
+    }
+}
